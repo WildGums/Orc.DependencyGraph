1f40710f035c3ee7bf2a5b2a8318376dde17cce1
Geert van Horrik <geert@catenalogic.com>
2018-04-30 19:29:39 +0200

    Migrate test project to new project structure

--- src/Orc.DependencyGraph.Tests/PublicApiFacts.cs
+++ src/Orc.DependencyGraph.Tests/PublicApiFacts.cs
@@ -7,13 +7,14 @@
 
 namespace Orc.DependencyGraph.Tests
 {
+    using System.Runtime.CompilerServices;
     using ApiApprover;
     using NUnit.Framework;
 
     [TestFixture]
     public class PublicApiFacts
     {
-        [Test]
+        [Test, MethodImpl(MethodImplOptions.NoInlining)]
         public void Orc_DependencyGraph_HasNoBreakingChanges()
         {
             var assembly = typeof(GraphGenerator).Assembly;
