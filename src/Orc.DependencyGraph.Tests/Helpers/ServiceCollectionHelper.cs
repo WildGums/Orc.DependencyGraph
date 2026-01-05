e7c302ded070eeb6d8f12a0803fb6aa50484029b
Geert van Horrik <geert@catenalogic.com>
2026-01-05 10:59:46 +0100

    Upgrade to Catel 7

---
+++ src/Orc.DependencyGraph.Tests/Helpers/ServiceCollectionHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Orc.DependencyGraph.Tests
+{
+    using Catel;
+    using Microsoft.Extensions.DependencyInjection;
+    using Orc.DependencyGraph;
+
+    internal static class ServiceCollectionHelper
+    {
+        public static IServiceCollection CreateServiceCollection()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddLogging();
+            serviceCollection.AddCatelCore();
+            serviceCollection.AddOrcDependencyGraph();
+
+            return serviceCollection;
+        }
+    }
+}
--- src/Orc.DependencyGraph/ModuleInitializer.cs
+++ src/Orc.DependencyGraph/ModuleInitializer.cs
@@ -1,7 +1,4 @@
-using System.Runtime.CompilerServices;
-using Catel.IoC;
-using Catel.Services;
-using Orc.DependencyGraph;
+﻿using System.Runtime.CompilerServices;
 
 /// <summary>
 /// Used by the ModuleInit. All code inside the Initialize method is ran as soon as the assembly is loaded.
@@ -14,11 +11,5 @@ public static class ModuleInitializer
     [ModuleInitializer]
     public static void Initialize()
     {
-        var serviceLocator = ServiceLocator.Default;
-
-        // TODO: register services here
-
-        var languageService = serviceLocator.ResolveRequiredType<ILanguageService>();
-        languageService.RegisterLanguageSource(new LanguageResourceSource("Orc.DependencyGraph", "Orc.DependencyGraph.Properties", "Resources"));
     }
 }
